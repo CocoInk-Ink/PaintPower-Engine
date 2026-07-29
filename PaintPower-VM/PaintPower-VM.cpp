8ff727b3f2f907302f79a693efa8e285f9385568
CocoBox84 <ninoelbambino2010@gmail.com>
2026-07-29 15:07:09 -0400

    Added C++

---
+++ PaintPower-VM/PaintPower-VM.cpp
@@ -0,0 +1,12 @@
+﻿// PaintPower-VM.cpp : Defines the entry point for the application.
+//
+
+#include "PaintPower-VM.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
