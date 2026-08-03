40269b8245d5fda6147c02aef4650a31642ef11e
CocoBox84 <ninoelbambino2010@gmail.com>
2026-08-02 20:46:49 -0400

    Removed C++

--- PaintPower-VM/PaintPower-VM.h
+++
@@ -1,8 +0,0 @@
-﻿// PaintPower-VM.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
