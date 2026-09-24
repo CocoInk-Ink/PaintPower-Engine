95dc52b4920beba76d4d4897ebabd8a9a288be5b
CocoBox84 <ninoelbambino2010@gmail.com>
2026-09-23 22:38:15 -0400

    Updated ActionBar

--- PaintPower/src/Editors/ActionBar.axaml.cs
+++ PaintPower/src/Editors/ActionBar.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Controls;
 using PaintPower.Templates.FileTemplates;
 using Toolbox;
 using Toolbox.Accessibility.Translation;
+using Toolbox.Logging;
 
 namespace PaintPower.Editors;
 
@@ -37,13 +38,19 @@ public partial class ActionBar : TranslatableControl
 		var runButton = DefaultParts.RunButton;
 		var buildAndRunButton = DefaultParts.BuildAndRunButton;
 
+		buildButton.Classes.Add("white");
+
 		buildButton.Content = Translator.Map("Build");
 		runButton.Content = Translator.Map("Run last successful build");
 		buildAndRunButton.Content = Translator.Map("Build and Run");
 
-		buildButton.Click += (_, _) => {};
-		runButton.Click += (_, _) => {};
-		buildAndRunButton.Click += (_, _) => {};
+		buildButton.Click += (_, _) => { Log.QuickLog("Build clicked"); };
+		runButton.Click += (_, _) => { Log.QuickLog("Run clicked"); };
+		buildAndRunButton.Click += (_, _) => { Log.QuickLog("Build and run"); };
+
+		buildButton.Margin = new Thickness(2, 1);
+		runButton.Margin = new Thickness(2, 1);
+		buildAndRunButton.Margin = new Thickness(2, 1);
 
 		buildButton.AddTranslateHandler(() =>
 		{
